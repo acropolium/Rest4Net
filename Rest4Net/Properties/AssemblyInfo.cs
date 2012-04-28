424346e6581cd31cee24cafb8e89ee16cea39594
Oleksiy Glib <oleksiy.glib@gmail.com>
2012-04-28 17:40:24 +0300

    NuGet added

--- Rest4Net/Properties/AssemblyInfo.cs
+++ Rest4Net/Properties/AssemblyInfo.cs
@@ -2,14 +2,11 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Rest4Net")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Library to develop rest service accessors smoothly")]
 [assembly: AssemblyCompany("Acropolium")]
 [assembly: AssemblyProduct("Rest4Net")]
-[assembly: AssemblyCopyright("Copyright © Oleksiy Glib 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Oleksii Glib 2010-2012")]
 [assembly: ComVisible(false)]
 [assembly: Guid("87fcb0e3-fb7d-450c-be7e-98db205bf89f")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.2.0")]
+[assembly: AssemblyFileVersion("0.8.2.0")]
