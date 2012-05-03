c6c9f3c4ddf68342ad6b56c13b24a8525fce3b33
Oleksiy Glib <oleksiy.glib@gmail.com>
2012-05-03 20:13:19 +0300

    Merge fixes

--- src/Rest4Net.Tests/HelperStubs/FailedDomainProvider.cs
+++ src/Rest4Net.Tests/HelperStubs/FailedDomainProvider.cs
@@ -1,18 +1,17 @@
-using System;
 using Rest4Net.Protocols;
 
 namespace Rest4Net.Tests.HelperStubs
 {
-	internal class FailedDomainProvider : RestServiceAccessProvider
+	internal class FailedDomainProvider : RestApiProvider
 	{
 		public FailedDomainProvider() : base(new Https("www.some-nonono-domain.com")) {}
 
-        public CommandResult Get()
-        {
-            return Cmd("/v1")
-                .WithParameter("q", "lectures")
-                .Execute();
-        }
+		public CommandResult Get()
+		{
+			return Cmd("/v1")
+				.WithParameter("q", "lectures")
+				.Execute();
+		}
 	}
 }
 
--- src/Rest4Net.Tests/HelperStubs/GoogleCustomSearch.cs
+++ src/Rest4Net.Tests/HelperStubs/GoogleCustomSearch.cs
@@ -1,19 +1,18 @@
-using System;
 using Rest4Net.Protocols;
 
 namespace Rest4Net.Tests.HelperStubs
 {
-	internal class GoogleCustomSearch : RestServiceAccessProvider
-    {
-        public GoogleCustomSearch() : base(new Https("www.googleapis.com")) {}
+	internal class GoogleCustomSearch : RestApiProvider
+	{
+		public GoogleCustomSearch() : base(new Https("www.googleapis.com")) {}
 
-        public CommandResult Get()
-        {
-            return Cmd("/customsearch/v1")
-                .WithParameter("cx", "017576662512468239146:omuauf_lfve")
-                .WithParameter("q", "lectures")
-                .Execute();
-        }
-    }
+		public CommandResult Get()
+		{
+			return Cmd("/customsearch/v1")
+				.WithParameter("cx", "017576662512468239146:omuauf_lfve")
+				.WithParameter("q", "lectures")
+				.Execute();
+		}
+	}
 }
 
