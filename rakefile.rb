0649ae5b5d4c598a2d56130d606dae7bcc4bbfaa
Oleksiy Glib <oleksiy.glib@gmail.com>
2012-05-04 19:09:29 +0300

    NuGet spec changed

--- rakefile.rb
+++ rakefile.rb
@@ -79,6 +79,7 @@ task :nuget_publish do |task|
     nupkgs.each do |nupkg| 
         puts "Pushing #{nupkg}"
         nuget_push = NuGetPush.new
+        nuget_push.command = "tools/nuget/nuget.exe"
         nuget_push.package = File.expand_path(nupkg)
         nuget_push.create_only = false
         nuget_push.execute
